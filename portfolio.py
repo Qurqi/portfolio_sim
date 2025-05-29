58a9cd458b1162a00c60f28ee3ddc2014252717b
Qurqi <125725390+Qurqi@users.noreply.github.com>
2025-05-29 00:23:48 -0700

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -7,25 +7,26 @@ import time
 
 class portfolio:
 
-    def __init__(self,holdings: pd.DataFrame,buy_sell_signals: pd.DataFrame):
+    def __init__(self,holdings: Dict,buy_sell_signals: pd.DataFrame):
         ##
         #  Initializes a portfolio object with the following attributes:
         #
-        #  :param holdings: dictionary containing {ticker:{amount:date_range} } triplets, where ticker is the stock ticker and amount is the number of shares held, and date_range is the range of dates which we wnt to 
-        #  :param index_per: dictionary containing {ticker:index_%} pairs, where index_% is the percentage of the index that the ticker represents
-        #  :param index_cw: dictionary containing {ticker:cap_weight} pairs, where cap_weight is the capitalization weight of the ticker within the portfolio's index
-        #  :param index: dictionary containing { 'index' : index_value}, where index_value is the total value of the portfolio's index
+        #  :param holdings: dictionary containing {ticker: amount} pairs, where ticker is the stock ticker and amount is the number of shares held
+        #  :param index: dataframe containing the indexes closing price data. the column name is index
         #  :param buy_sell: DataFrame containing buy/sell signals for each stock in the portfolio retrieved from model predictions
-        #  :param stats: dictionary containing portfolio statistics such as maximum drawdown, the corresponding recovery period, PnL, Win Loss Ratio, and possibly more to be added
+        #  :param data: dataframe containing the closing price data for each stock in the portfolio. ticker names are the column names
+        #  :param stats: dictionary containing portfolio statistics such as maximum drawdown, the corresponding recovery period, PnL, Win Loss Ratio, 
+        #                and the capitalization weight of each stock in the portfolio. Stat names are the indexes and ticker names are the column names
         ## 
-        if holdings == None:
-            self.holdings = pd.DataFrame(columns=['ticker', 'amount','dd_max','dd_rec']) ## DataFrame to hold the holdings of the portfolio
+        if holdings.empty:
+            self.holdings = {}
         else:
             self.holdings = holdings
         self.index = pd.DataFrame(columns = ['index']) ## DataFrame to hold the index of the portfolio
-        self.index_cw = pd.DataFrame(columns=['index_cw']) ## dict = {'ticker': index_%}
         self.buy_sell = buy_sell_signals
         self.data = pd.DataFrame()
+        self.stats = pd.DataFrame(index = ['dd_max', 'dd_rec', 'PnL', 'win_loss_ratio','cap_weight']) # append more stats as we add them
+
 
     def add_stocks(self, ticker, amount, tickers: Dict = None):
            '''
@@ -37,14 +38,10 @@ class portfolio:
            :param tickers: optional input dictionary containing a set of {ticker:amount} pairs for convenient portfolio initialization
 
            '''
-           if tickers != None:
-               self.holdings.update(tickers)
-           else:
-               self.holdings.update({ticker: amount})
-   
+           
 
 
-    def make_index(index,index_cw,index_per,holdings,time_range):
+    def make_index(self,time_range):
         '''
         Constructs a capitalization weighted index using the tickers and amounts in holdings with yfinance data
 
@@ -56,22 +53,7 @@ class portfolio:
                            start date and end_date must be in the format 'YYYY-MM-DD'
 
         '''
-
-        ## Pull prices for each ticker name and construct cap_weight, index, and index_% dictionaries
-        ## Data pulled in df format
-
-        for ticker in holdings.keys():
-            price = yf.download(ticker, start= time_range[0], end=time_range[1]) ## retrieve price data 
-            cap_weight = price * ticker.get(ticker) ## calculate cap weight for each ticker, price of ticker * amount
-            index_cw.update({ticker: cap_weight}) ## store cap_weight in dictionary for later use 
-            index.update( {"index": (cap_weight+index.get("index")) } ) ## update index value for each cap_weight value
-        ## Have to wait for index value to be finalized before calculating index_%
-        for ticker in index_cw.keys():
-            index_per.update( {ticker: (index_cw.get(ticker)*100/index.get("index")) } ) ## divide each cap_weight by index value to get index_%
-      
-        
-
-
+        pass
 
 
     def update_holdings(self, update: Dict):
