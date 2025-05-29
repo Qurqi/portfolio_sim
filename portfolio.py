0c649e7f0549dfe05822e9597c61d5f0ad3022c0
Qurqi <125725390+Qurqi@users.noreply.github.com>
2025-05-28 21:20:19 -0700

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -7,7 +7,7 @@ import time
 
 class portfolio:
 
-    def __init__(self,holdings: Dict,buy_sell_signals: pd.DataFrame):
+    def __init__(self,holdings: pd.DataFrame,buy_sell_signals: pd.DataFrame):
         ##
         #  Initializes a portfolio object with the following attributes:
         #
@@ -19,16 +19,14 @@ class portfolio:
         #  :param stats: dictionary containing portfolio statistics such as maximum drawdown, the corresponding recovery period, PnL, Win Loss Ratio, and possibly more to be added
         ## 
         if holdings == None:
-            self.holdings = {} ## dict = {'ticker': {amount:date_range} } nested dictionaries with ticker amount and date_range groups
+            self.holdings = pd.DataFrame(columns=['ticker', 'amount']) ## DataFrame to hold the holdings of the portfolio
         else:
             self.holdings = holdings
-
-        self.index_per = {} ## dict = {'ticker': index_%}
-        self.index_cw = {} ## dict = {'ticker': cap_weight} 
-        self.index = {'index':0.0} ## dict = { 'index' : index_value}
+        self.index = pd.DataFrame(columns = ['index']) ## DataFrame to hold the index of the portfolio
+        self.index_cw = pd.DataFrame(columns=['index_cw']) ## dict = {'ticker': index_%}
         self.buy_sell = buy_sell_signals
-        self.stats = {} ## dict = {'dd_max': 0, 'dd_rec': 0, ...} stats for index
-        self.data = {} ## dict = {'ticker': DataFrame} where DataFrame contains stock data for ticker, including Open, High, Low, Close, Adj_close, Volume
+        self.stats = pd.DataFrame(columns=['dd_max','dd_rec'])
+        self.data = pd.DataFrame()
 
     def add_stocks(self, ticker, amount, tickers: Dict = None):
            '''
@@ -56,19 +54,34 @@ class portfolio:
         :param index_per: dictionary containing {ticker:index_%} pairs
         :param holdings: dictionary containing {ticker:amount} pairs
         :param time_range: 2 entry list containing [start_date, end_date] of stock data to use in index. 
-                          start date and end_date must be in the format 'YYYY-MM-DD'
+                           start date and end_date must be in the format 'YYYY-MM-DD'
 
         '''
 
         ## Pull prices for each ticker name and construct cap_weight, index, and index_% dictionaries
         ## Data pulled in df format
-       
+
+        for ticker in holdings.keys():
+            price = yf.download(ticker, start= time_range[0], end=time_range[1]) ## retrieve price data 
+            cap_weight = price * ticker.get(ticker) ## calculate cap weight for each ticker, price of ticker * amount
+            index_cw.update({ticker: cap_weight}) ## store cap_weight in dictionary for later use 
+            index.update( {"index": (cap_weight+index.get("index")) } ) ## update index value for each cap_weight value
+        ## Have to wait for index value to be finalized before calculating index_%
+        for ticker in index_cw.keys():
+            index_per.update( {ticker: (index_cw.get(ticker)*100/index.get("index")) } ) ## divide each cap_weight by index value to get index_%
+      
+        
+
+
+
 
     def update_holdings(self, update: Dict):
         '''   
         Updates the holdings dictionary with ticker:amount pairs in update dictionary 
 
         '''
+
+
         pass
 
     def update_index(self):
