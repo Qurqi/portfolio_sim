66339e89e8da43ed40419a1a722587e43b01058e
Qurqi <125725390+Qurqi@users.noreply.github.com>
2025-05-29 02:47:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from stats import drawdown_analysis
 
 # Example Usage
 if __name__ == "__main__":
-    holdings = {'AAPL': 10, 'MSFT': 5}
+    holdings = {'AAPL':7, 'MSFT':6, 'AMZN':2}
+
     buy_sell_signals = pd.DataFrame()
 
     pf = portfolio(holdings, buy_sell_signals)
-    pf.add_stocks('GOOG', 7)
+    pf.make_index(['2020-01-01', '2023-01-01'])
+    drawdown_analysis(pf)
 
-    portfolio.make_index(pf.index, pf.index_cw, pf.index_per, pf.holdings, ['2024-01-01', '2024-01-03'])
 
-    print("Holdings:", pf.holdings)
-    print("Index:", pf.index)
-    print("Cap Weights:", pf.index_cw)
-    print("Index %:", pf.index_per)
