31d781dcacd09690251ceb00ad5ad236f4738f15
Qurqi <125725390+Qurqi@users.noreply.github.com>
2025-05-31 18:41:54 -0700

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,6 @@
+
 import pandas as pd
+import portfolio as pf
 
 def drawdown_analysis(self):
     '''
@@ -6,75 +8,60 @@ def drawdown_analysis(self):
     :param stats: Dataframe, containing the portfolio's maximum drawdown and corresponding recovery period, to be updated
     :param index: Dataframe containing the index value at each time period.
     '''
-    ## verify dd_max and dd_rec are not NaN
-    if self.stats.empty:
-        cols = list(self.holdings.keys()) + ['index']
-        self.stats = pd.DataFrame(index = ['dd_max', 'dd_rec'], columns = cols)
-        self.stats.fillna(0, inplace=True)
-    ## store index[0] as intial base value
-    peak = self.index.iat[0] #store index value 0
-    i_peak = 0
-    
-    for i in range(1,self.index.shape[0]):
-      
-      ## Check for recovery time and calculate drawdown if so. Check all Positive slope line points 
-      #  to see if they are greater than the peak as this is where the recovery happens.
-      if (self.index.iat[i-1] < self.index.iat[i]) and (self.index.iat[i+1] > self.index.iat[i]):
-        if self.index.iat[i] > peak:
-          trough = min(self.index.iat[i], self.index.iat[i_peak])  # Find the minimum value between peak and current index
-          dd = (peak - trough)*100/peak
-          if dd > self.stats.at['dd_max','index']:
-            dd_rec = i - i_peak
-            self.stats.at['dd_rec','index'] = dd_rec
-            self.stats.at['dd_max','index'] = dd
-          
-      ## Find local max. Find all points with negative concavity and check their relative heights
-      if (self.index.iat[i-1] < self.index.iat[i]) and (self.index.iat[i+1] < self.index.iat[i]):
-        if self.index.iat[i] > peak:
-          peak = self.index.iat[i]
-          i_peak = i
+    if self.stats.index.name != 'dd_max':
+      #append 0 row to stats dataframe
+      self.stats = self.stats._append(pd.Series(name='dd_max'))
+      self.stats.loc['dd_max'] = 0.0  # Initialize the dd_max row with zeros for all tickers
+    if self.stats.index.name != 'dd_rec':
+      #append 0 row to stats dataframe
+      self.stats = self.stats._append(pd.Series(name='dd_rec'))
+      self.stats.loc['dd_rec'] = 0.0  # Initialize the dd_max row with zeros for all tickers
 
-      for ticker in self.stats.columns [:-1]:  # Exclude 'index' column
-        peak = self.data[ticker].iat[0] #store index value 0
-        i_peak = 0
     
-        for i in range(1,self.data[ticker].shape[0]-1):
-          
-          if (self.data[ticker].iat[i-1] < self.data[ticker].iat[i]) and (self.data[ticker].iat[i+1] > self.data[ticker].iat[i]):
-            if self.data[ticker].iat[i] > peak:
-          
-              trough = min(self.data[ticker].iloc[i_peak:i])  # Find the minimum value between peak and current index
-              dd = (peak - trough)*100.0/peak
-              
-              if dd > self.stats.at['dd_max',ticker]:
-                dd_rec = i - i_peak
-                print(dd_rec, dd, ticker)
-                self.stats.at['dd_rec',ticker] = dd_rec 
-                self.stats.at['dd_max',ticker] = dd
+    for ticker in self.stats.columns:  
+      peak = self.data[ticker].iat[0] #store index value 0
+      i_peak = 0
+  
+      for i in range(1,self.data[ticker].shape[0]-1):
+        
+        if (self.data[ticker].iat[i-1] < self.data[ticker].iat[i]) and (self.data[ticker].iat[i+1] > self.data[ticker].iat[i]):
+          if self.data[ticker].iat[i] > peak:
+        
+            trough = min(self.data[ticker].iloc[i_peak:i])  # Find the minimum value between peak and current index
+            dd = (peak - trough)*100.0/peak
+            
+            if dd > self.stats.at['dd_max',ticker]:
+              dd_rec = i - i_peak
+              self.stats.at['dd_rec',ticker] = dd_rec 
+              self.stats.at['dd_max',ticker] = dd
+        if (self.data[ticker].iat[i-1] < self.data[ticker].iat[i]) and (self.data[ticker].iat[i+1] < self.data[ticker].iat[i]):
+          if self.data[ticker].iat[i] > peak:
+            peak = self.data[ticker].iat[i]
+            i_peak = i
 
-          if (self.data[ticker].iat[i-1] < self.data[ticker].iat[i]) and (self.data[ticker].iat[i+1] < self.data[ticker].iat[i]):
-            if self.data[ticker].iat[i] > peak:
-              peak = self.data[ticker].iat[i]
-              i_peak = i
-          
-    print("Drawdown Analysis Complete")
-    print(self.stats)
-    
-def win_loss_ratio(self, buy_sell):
+def win_loss_ratio(self):
     '''
     Calculates the Win/Loss Ratio, comparing the number of winning trades to losing trades.
     :return : float: Win/Loss ratio value.
     '''
-
+    if self.stats.index.name != 'win_loss_ratio':
+      #append 0 row to stats dataframe
+      self.stats = self.stats._append(pd.Series(name='win_loss_ratio'))
+      self.stats.loc['win_loss_ratio'] = 0.0  # Initialize the win_loss_ratio row with zeros for all tickers
     pass
   
-def calculate_PnL(stats,index,buy_sell):
-      '''
-      Profit & Loss (P&L): Track realized if we were to follow what this model predicts.
+def calculate_PnL(self):
+    '''
+    Profit & Loss (P&L): Track realized if we were to follow what this model predicts.
 
-      :param index: Dataframe containing the index value at each time period.
-      :param buy_sell: Dataframe containing the buy/sell signals at each time period.
-      :return: float: Positive/Negative value if profit/loss was made at the end of this period
-      '''
+    :param index: Dataframe containing the index value at each time period.
+    :param buy_sell: Dataframe containing the buy/sell signals at each time period.
+    :return: float: Positive/Negative value if profit/loss was made at the end of this period
 
-      pass
+    '''
+    if self.stats.index.name != 'win_loss_ratio':
+      #append 0 row to stats dataframe
+      self.stats = self.stats._append(pd.Series(name='win_loss_ratio'))
+      self.stats.loc['win_loss_ratio'] = 0.0  # Initialize the win_loss_ratio row with zeros for all tickers
+      
+    pass
